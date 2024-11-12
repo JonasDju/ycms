1d97d138e3f9ae8ee62c82b0cc820abd9dcc439c
BurakKayabasi <burak.kayabasi@rwth-aachen.de>
2024-11-12 20:21:12 +0100

    feat backend-integration
    Co-authored-by: Jojo <Jojo200401@users.noreply.github.com>

--- ycms/cms/views/floor/floor_view.py
+++ ycms/cms/views/floor/floor_view.py
@@ -1,25 +1,36 @@
 from django.views.generic import TemplateView
 
+from ...models import Floor, Ward
+
 
 class FloorView(TemplateView):
     template_name = "floor/floor.html"
-    floor = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # Aktuelle Etage aus der URL-Parameter abrufen
-        current_floor = self.request.GET.get(
-            "floor", 1
-        )  # Standardwert 1, wenn nicht gesetzt
-        context["current_floor"] = int(current_floor)
-        context["total_floors"] = self.get_number_of_floor()  # Anzahl der Etagen
-        context["upper_bound_floor"] = self.floor - 1
+        # Hol die aktuelle Etage
+        current_floor_order = self.request.GET.get("floor", None)
+        if current_floor_order:
+            current_floor = Floor.objects.filter(order=current_floor_order).first()
+            current_floor_id = current_floor.id
+        else:
+            current_floor = Floor.objects.first()
+            current_floor_id = current_floor.id
 
-        # Erstelle eine Liste der Etagen und kehre sie um
-        context["floors"] = list(reversed(range(0, context["total_floors"])))
+        # Bereite Daten vor
+        context["current_floor"] = current_floor
+        sorted_floors = sorted(Floor.objects.all(), key=lambda x: x.order, reverse=True)
+        context["floors"] = sorted_floors
+        dict_floor_order = {floor.order: floor.name for floor in sorted_floors}
+
+        # Berechne den nächsten Etagen-Order
+        next_floor_order = current_floor.order + 1
+        prev_floor_order = current_floor.order - 1
+        context["next_floor_name"] = dict_floor_order.get(next_floor_order, None)
+        context["prev_floor_order"] = dict_floor_order.get(prev_floor_order, None)
+
+        # Ab hier wollen wir weinen
+        context["floor_wards"] = Ward.objects.filter(floor=current_floor_id)
 
         return context
-
-    def get_number_of_floor(self):
-        return self.floor  # Gibt die Anzahl der Etagen zurück
