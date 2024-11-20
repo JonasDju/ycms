2c7da10fb510263af3643e0a845863b87c2359ca
Jojo <joana.schmidt@rwth-aachen.de>
2024-11-20 10:27:05 +0100

    add floor overlay

--- ycms/cms/urls/protected.py
+++ ycms/cms/urls/protected.py
@@ -12,7 +12,8 @@ from ..views import (
     user_settings_view,
     ward,
 )
-from ..views.floor.floor_view import FloorView  # Importiere die FloorView hier
+from ..views.floor.floor_view import FloorView
+from ..views.floor.add_floor import add_floor
 from ..views.utility.autocomplete import autocomplete_icd10, autocomplete_patient
 
 urlpatterns = [
@@ -136,4 +137,5 @@ urlpatterns = [
     ),
     path("settings/", user_settings_view.UserSettingsView.as_view(), name="settings"),
     path("floor/", FloorView.as_view(), name="floor"),
+    path("add_floor/", add_floor, name="add_floor")
 ]
