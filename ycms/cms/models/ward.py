cabdc3a6e7ce6549e179ea50098b3089adec3328
Jamie Anike Heikrodt <anike.heikrodt@rwth-aachen.de>
2024-11-26 17:26:33 +0100

    Ward nicknames (#41)
    * test: Add ward nicknames to test data
    
    * Feat: Add nickname to ward model
    
    * Feat: Ward nickname migration to database
    
    * Fix: Migration for ward nickname
    
    * Fix: Migration and model for ward nickname
    
    * Refactor: Remove ward number
    
    * fix: migrations failed because of modified initial migration
    
    ---------
    
    Co-authored-by: Jamie Anike Heikrodt <anikeheikrodt@rwth-aachen.de>

--- ycms/cms/models/ward.py
+++ ycms/cms/models/ward.py
@@ -17,12 +17,6 @@ class Ward(AbstractBaseModel):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=current_or_travelled_time, null=False)
     updated_at = models.DateTimeField(auto_now=True, null=False)
-    ward_number = models.CharField(
-        unique=True,
-        max_length=32,
-        verbose_name=_("ward number"),
-        help_text=_("Number of the ward"),
-    )
     floor = models.IntegerField(
         verbose_name=_("floor"),
         help_text=_("Floor on which the nurse station for this ward is located"),
@@ -31,6 +25,13 @@ class Ward(AbstractBaseModel):
         max_length=32,
         verbose_name=_("ward name"),
         help_text=_("Name this ward is commonly referred to by"),
+    )
+    nickname = models.CharField(
+        max_length=32,
+        verbose_name=_("ward nickname"),
+        help_text=_("Nickname of this ward"),
+        blank=True,
+        default="",
     )
 
     @cached_property
@@ -94,7 +95,11 @@ class Ward(AbstractBaseModel):
         :return: A readable string representation of the ward
         :rtype: str
         """
-        return f"{self.name} (ward {self.ward_number})"
+
+        if (self.nickname != None and self.nickname != ""):
+            return f"{self.nickname}"
+        else:
+            return f"{self.name}"
 
     def get_repr(self):
         """
@@ -104,7 +109,7 @@ class Ward(AbstractBaseModel):
         :return: The canonical string representation of the ward
         :rtype: str
         """
-        return f"<Ward (number: {self.ward_number})>"
+        return f"<Ward (name: {self.name})>"
 
     class Meta:
         verbose_name = _("ward")
